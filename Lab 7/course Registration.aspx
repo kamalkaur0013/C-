6d6f1cc142c2290b3a50e99a07d5b9d61fbe5f77
kamalkaur0013 <32466602+kamalkaur0013@users.noreply.github.com>
2018-10-23 15:42:59 -0400

    Add files via upload

---
+++ Lab 7/course Registration.aspx
@@ -0,0 +1,41 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="course Registration.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Algonquin College Course Registration</title>
+    
+    <link href="App_Themes/SiteStyles.css" rel="stylesheet" />
+</head>
+<body>
+    <h1 >Algonquin College Course Registration</h1>
+
+    <form id="form1" runat="server" visible ="true">
+        <div><asp:Panel ID="pnlStudentType" CssClass="displayInline marginLeft15" runat="server">
+            <asp:Label ID="lblStudentName" runat="server" Text="Student Name"></asp:Label>
+            <asp:TextBox ID="txtStudentName" runat="server"></asp:TextBox>
+            <asp:RadioButton ID="btuFulltime"  GroupName="radio" runat="server" Text="Full Time" />
+            <asp:RadioButton ID="btuparttime"  GroupName="radio" runat="server" Text="Part Time" />
+            <asp:RadioButton ID="btucoop" GroupName="radio" runat="server" Text="Co-op" /> </asp:Panel>
+            
+        </div>
+        <h2>Fallowing courses are currently available for registration </h2><br />
+         <asp:Label ID="lblError" runat="server" CssClass =" error" Visible ="False"></asp:Label>
+      
+        <asp:CheckBoxList ID="CheckBoxList" runat="server">
+        </asp:CheckBoxList>
+       
+        
+        <asp:Button ID="btnSubmit" runat="server" CssClass="button" Text="Submit" OnClick =" btnSubmit_Click" />
+          
+    </form>
+        <div id="studentInfo" runat="server">
+        <p>Thank you <asp:Label ID="lblStudentInfoName" CssClass="emphsis" runat="server"></asp:Label>, for using our online registration system.</p>
+        <p>You have been registered as a <asp:Label ID="lblStudentInfoType" CssClass="distinct" runat="server"></asp:Label> for the following courses</p>
+        <asp:Table ID="tblCourses" CssClass="table" runat="server"></asp:Table>
+    </div>
+        
+    
+</body>
+</html>
---
+++ Lab 7/course Registration.aspx.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+        lblError.Visible = true;
+        studentInfo.Visible = false;
+        form1.Visible = true;
+
+
+
+        // if i want to display my list before submit button
+        if (IsPostBack == false)
+        {
+            // creating new list 
+           // List<Course> CheckList = new List<Course>();
+
+            // calling given list contents in new list
+            //CheckList = Helper.GetCourses();
+            
+            // to display the list
+            foreach (Course c in Helper.GetCourses())
+            {
+                // calling ginen method from course class
+                c.ToString();
+
+                CheckBoxList.Items.Add(c.ToString());
+
+
+            } // END of foreach 
+
+        }//end of Ispost
+
+
+
+    }
+
+    protected void ValidateForm(){
+
+        var studentName = txtStudentName.Text;
+
+        var studentType = pnlStudentType.Controls.OfType<RadioButton>().FirstOrDefault(r => r.Checked);
+        if (studentType == null)
+            return;
+
+        Student student =null;
+
+
+        if (studentType == btuFulltime)
+        {
+            student = new FullTimeStudent(studentName);
+         }
+
+        else if(studentType == btuparttime)
+        {
+            student = new PartTimeStudent(studentName);
+        }
+        else
+        {
+            student = new CoopStudent(studentName);
+        }
+        var error = false;
+
+        for (var i = 0; i < CheckBoxList.Items.Count; i++)
+        {
+            if (!CheckBoxList.Items[i].Selected)
+                continue;
+            try
+            {
+                student.addCourse(Helper.GetCourses()[i]);//add course is taken from full time class
+            }
+            catch(Exception ex)
+            {
+                lblError.Text = ex.Message;
+                lblError.Visible = true;
+                error = true;
+            }
+           
+            
+           // lblError.Visible = false;
+        }
+
+        if (!error)
+        {
+            StudentInfo(student, studentType.Text);
+        }
+
+
+    }//validate form
+
+
+    protected void btnSubmit_Click(object sender, EventArgs e)
+    {
+        
+        ValidateForm();
+        //form1.Visible = false;
+    }
+
+    protected void StudentInfo(Student student, string type)
+    {
+        lblStudentInfoName.Text = student.Name;
+        lblStudentInfoType.Text = student.ToString();
+
+        studentInfo.Visible = true;
+
+
+        var tableHead = new TableHeaderRow();
+        tblCourses.Rows.Add(tableHead);// tblCourse is id from aspx file of table
+
+        var tableHeadCellCode = new TableHeaderCell();
+        var tableHeadCellTitle = new TableHeaderCell();
+        var tableHeadCellHours = new TableHeaderCell();
+        var tableHeadCellFee = new TableHeaderCell();
+
+        tableHeadCellCode.Text = "Course Code";
+        tableHeadCellTitle.Text = "Course Title";
+        tableHeadCellHours.Text = "Weekly Hours";
+        tableHeadCellFee.Text = "Fee Payable";
+
+        tableHead.Cells.Add(tableHeadCellCode);
+        tableHead.Cells.Add(tableHeadCellTitle);
+        tableHead.Cells.Add(tableHeadCellHours);
+        tableHead.Cells.Add(tableHeadCellFee);
+
+        foreach (Course course in student.getEnrolledCourses())
+        {
+            // Start creating the table rows
+            var tableRow = new TableRow();
+            tblCourses.Rows.Add(tableRow);
+
+            // Now the table cells
+            var tableCellCode = new TableCell();
+            var tableCellTitle = new TableCell();
+            var tableCellHours = new TableCell();
+            var tableCellFee = new TableCell();
+
+            tableCellCode.Text = course.Code;
+            tableCellTitle.Text = course.Title;
+            tableCellHours.Text = Convert.ToString(course.WeeklyHours);
+            tableCellFee.Text = Convert.ToString(String.Format("{0:C0}", course.Fee));
+
+            tableRow.Cells.Add(tableCellCode);
+            tableRow.Cells.Add(tableCellTitle);
+            tableRow.Cells.Add(tableCellHours);
+            tableRow.Cells.Add(tableCellFee);
+        }
+        // Generate the table footer
+        var tableFoot = new TableRow();
+        tblCourses.Rows.Add(tableFoot);
+
+        var tableFootTotalLabel = new TableCell();//total text
+        var tableFootTotalRows = new TableCell();//total hours
+        var tableFootTotal = new TableCell();//total fee
+
+        tableFootTotalLabel.Text = "Total";
+        tableFootTotalLabel.HorizontalAlign = HorizontalAlign.Right;
+        tableFootTotalLabel.ColumnSpan = 2;
+        tableFootTotalRows.Text = Convert.ToString(student.totalWeeklyHours());
+        tableFootTotal.Text = Convert.ToString(String.Format("{0:C0}", student.feePayable()));
+
+        tableFoot.Cells.Add(tableFootTotalLabel);
+        tableFoot.Cells.Add(tableFootTotalRows);
+        tableFoot.Cells.Add(tableFootTotal);
+
+
+
+    }
+
+
+
+
+
+
+
+
+}//end of class
+
+
+
