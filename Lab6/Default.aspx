d4d15cd25cb533f57b83c658686455eede815e87
kamalkaur0013 <32466602+kamalkaur0013@users.noreply.github.com>
2018-10-23 15:42:09 -0400

    Add files via upload

---
+++ Lab6/Default.aspx
@@ -0,0 +1,47 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="_Default" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>Statistic Calculator</title>
+</head>
+<body bgcolor ="f4c2c2">
+
+    <h1>Staticstic Calculator</h1>
+    <form id="form1" runat="server">
+        <div>
+            <asp:Label ID="lblInstructions" runat="server" Text="Enter three numbers and click Submit button to find out the statistics"></asp:Label>
+        </div>
+        <asp:Label ID="lblFirstNumber" runat="server" Text="First Number:"></asp:Label>
+        <asp:TextBox ID="txtFirstNumber" runat="server"></asp:TextBox><br />
+        
+        <asp:Label ID="lblSecondNumber" runat="server" Text="Second Number:"></asp:Label>
+        <asp:TextBox ID="txtSecondNumber" runat="server"></asp:TextBox><br />
+        
+        <asp:Label ID="lblThirdNumber" runat="server" Text="Third Number:"></asp:Label>
+        <asp:TextBox ID="txtThirdNumber" runat="server"></asp:TextBox><br />
+       
+        
+        <asp:Label ID="lblerror" runat="server"  ForeColor = "#FF3300 " Visible ="false" ></asp:Label><br />
+         <asp:Button ID="btnSubmit" runat="server" Text="Submit" /><br />
+            <div>
+            <h2>Statistics of the number you entered</h2></div>
+                
+        
+        
+            <asp:Label ID="lblMaximum" runat="server" Text="Maximum:"></asp:Label>
+          
+
+        
+        
+     
+        <asp:Label ID="lblMaxAnswer" runat="server" Text=" "></asp:Label><br />
+        
+        <asp:Label ID="lblMinimum" runat="server" Text="Minimum:"></asp:Label><br />
+        
+        <asp:Label ID="lblAverage" runat="server" Text="Average:"></asp:Label><br />
+        <asp:Label ID="lblTotal" runat="server" Text="Total:"></asp:Label>
+    </form>
+</body>
+</html>
---
+++ Lab6/Default.aspx.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        
+        if (IsPostBack)
+        {
+            try
+            {
+                var firstNumber = double.Parse(txtFirstNumber.Text);
+                var secondNumber = double.Parse(txtSecondNumber.Text);
+                var thirdNumber = double.Parse(txtThirdNumber.Text);
+
+               // ClearResult();
+
+                lblMaximum.Text = "Max is: " + GetMaxNumber(firstNumber, secondNumber, thirdNumber);
+                lblMinimum.Text = "Min is: " + GetMinNumber(firstNumber, secondNumber, thirdNumber);
+                lblAverage.Text = "Average is: " + GetAvgNumber(firstNumber, secondNumber, thirdNumber);
+                lblTotal.Text = "Total is: " + GetSumNumber(firstNumber, secondNumber, thirdNumber);
+
+                //ClearResult();
+            }
+            catch(Exception ex)
+
+            {
+               
+                lblerror.Text = ex.Message;
+                lblerror.Visible = true;
+               // ClearResult();
+
+
+            }
+
+        }
+       else
+        {
+            lblerror.Visible = false;
+        }
+      
+    }
+    protected double GetMaxNumber(double firstNumber, double secondNumber, double thirdNumber)
+    {
+        if (firstNumber > secondNumber)
+        {
+            if (firstNumber > thirdNumber)
+            {
+                return firstNumber;
+            }
+            else 
+
+            {
+                return thirdNumber;
+
+
+            }
+        }
+        else
+        
+        {
+            if (secondNumber > thirdNumber)
+                return secondNumber;
+            else
+                return thirdNumber;
+        }
+    }
+    protected double GetMinNumber(double firstNumber, double secondNumber, double thirdNumber)
+    {
+        if (firstNumber < secondNumber)
+        {
+            if (firstNumber <thirdNumber)
+            {
+                return firstNumber;
+            }
+            else
+
+            {
+                return thirdNumber;
+
+
+            }
+        }
+        else
+
+        {
+            if (secondNumber < thirdNumber)
+                return secondNumber;
+            else
+                return thirdNumber;
+        }
+    }
+    protected double GetAvgNumber(double firstNumber, double secondNumber, double thirdNumber)
+    {
+        return ((firstNumber + secondNumber + thirdNumber) / 3); 
+    }
+    protected double GetSumNumber(double firstNumber, double secondNumber, double thirdNumber)
+    {
+        return firstNumber + secondNumber + thirdNumber;
+    }
+       // private void ClearResult()
+       // {
+
+
+      
+        
+           // lblMaximum.Text += "";
+            //lblMinimum.Text += "";
+            //lblAverage.Text += "";
+            //lblTotal.Text += "";
+            //lblerror.Visible = false;
+     
+
+       // }
+    
+}
